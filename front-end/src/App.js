b6a9231e600154cf7b65f283280ec715452b60b4
Diego <diegojeda775@yahoo.com>
2024-02-29 20:33:27 -0800

    setting up frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -7,10 +8,27 @@ const router = createBrowserRouter([
     path: "/",
     element: <HomePage />,
     errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "dashboard",
+      },
+    ],
   },
 ]);
 
 function App() {
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
+
+  const abortControllerRef = useRef(null);
+
+  const loadDashboard = () => {
+    try {
+    } catch (error) {}
+  };
+
   return <RouterProvider router={router} />;
 }
 
